a16ff73199c3ce8d590e0bbe5f231fc7a8064f55
ant-droid <ant.dobr24@mail.ru>
2024-07-10 17:11:40 +0500

    add db

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+// Constants 
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+async function start(){
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.t3ffycd.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+        )
+
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+    } catch (error) {
+        console.log(error)        
+    }
+}
+
+start()
